9fbb5ad63cf3eee495610908b18240f103fc41a3
unknown <jesonkayleen1@gmail.com>
2026-04-15 10:40:44 +1000

    added flightNumber attribute, and added Booking class, and bookingNumber attribute.

--- CheckInSystem.java
+++ CheckInSystem.java
@@ -1,17 +1,16 @@
 class Flight {
+    String flightNumber;
+    
+}
 
+class Booking {
+    int bookingNumber;
 }
 
 class Passenger {
-    String name;
-    int age;
 
-    Passenger(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 }
 
 class CheckInSystem {
-
+    
 }
