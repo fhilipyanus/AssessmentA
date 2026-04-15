cac5965d656bdba0f924abbf0c7e1efaf7c25d2b
fhilipyanus <fhilipyanusschool@gmail.com>
2026-04-15 10:18:19 +1000

    added name and age attributes to the Passenger class, and created the passenger constructor

--- CheckInSystem.java
+++ CheckInSystem.java
@@ -1,10 +1,17 @@
 class Flight {
-    
+
 }
+
 class Passenger {
-    
+    String name;
+    int age;
+
+    Passenger(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
 class CheckInSystem {
-    
+
 }
